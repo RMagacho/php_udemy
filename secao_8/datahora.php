0a0dab93150355a2dfd44b9cb80fc751f35e7ed4
RMagacho <rpmagacho@gmail.com>
2022-04-08 08:14:36 -0300

    Data e Hora

---
+++ secao_8/datahora.php
@@ -0,0 +1,26 @@
+<?php
+    function pula(){
+        echo "<br>";
+    }
+    /**
+     * Trabalhando com Data e Hora
+     */
+
+    // echo date("d/mY h:i:s");
+    // pula();
+    // echo time();
+    // pula();
+
+    /**
+     * Padrões locale para llinux, padrão utf-8 e windows
+     */
+    // putenv('LANG=pt_BR.utf-8');
+    // putenv('LANGUAGE=pt_BR');
+    // $local = setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "portuguese");
+    // echo $local;
+    // pula();
+    // echo ucwords(strftime("%A %B"));
+
+
+
+?>
